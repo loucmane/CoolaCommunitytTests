f828a3167d021797843bc28d3fce5498d69dea8d
Loucmane <lookmanbenali@gmail.com>
2021-01-26 12:18:33 +0100

    first commit

---
+++ cypress/integration/Login.js
@@ -0,0 +1,27 @@
+describe("Login form", () => {
+
+    it("Can sign in", () => {
+
+        cy.visit("/");
+        cy.get("form");
+        cy.get('input[name="username"]').type("CoolUser").should("have.value", "CoolUser");
+        cy.get('input[name="password"]').type("123123123").should("have.value", "123123123");
+        cy.get("form").submit();
+        cy.url().should("include", "start").end();
+
+    });
+
+    it("Can't sign in with wrong credentials", () => {
+
+        cy.visit("/");
+        cy.get("form");
+        cy.get('input[name="username"]').type("CoolUser");
+        cy.get('input[name="password"]').type("asd");
+        cy.get("form").submit();
+        cy.contains("Fel användarnamn eller lösenord!").end();
+
+
+    });
+
+
+})
