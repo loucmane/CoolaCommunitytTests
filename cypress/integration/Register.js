5661e8ce260922c63a0ce0f669e99e56d5bcd809
FilipAlmstedt <filip.almstedt@almxer.se>
2021-01-27 18:58:44 +0100

    Added comments to Register.js

--- cypress/integration/Register.js
+++ cypress/integration/Register.js
@@ -1,6 +1,8 @@
 describe("Account registration form", () => {
     
+    //Test for an unsucessfull username valiadation. Check that the right url pops up when the user types in a username less than 3 characters or more than 12
     it("Username validation", () =>{
+
         cy.visit("/");
         cy.get("a").click();
 
@@ -18,6 +20,7 @@ describe("Account registration form", () => {
 
     });
 
+    //Test for unsucessfull email validation
     it("Email validation", () => {
 
         cy.visit("/");
@@ -25,36 +28,42 @@ describe("Account registration form", () => {
 
         cy.url().should('eq', 'http://127.0.0.1:5500/register.html');
 
+        //Check if the user types in an email without anything after the @ which is invalid
         cy.get("form");
         cy.get('input[name="username"]').type("CoolUser");
         cy.get('input[name="email"]').type("email@");
         cy.get("form").submit();
         cy.url().should('eq', 'http://127.0.0.1:5500/register.html?Felformatterad%20e-postadress!');
 
+        //Check if the user types in an email without anything after the . which is invalid
         cy.get("form");
         cy.get('input[name="username"]').type("CoolUser");
         cy.get('input[name="email"]').type("email@domain.");
         cy.get("form").submit();
         cy.url().should('eq', 'http://127.0.0.1:5500/register.html?Felformatterad%20e-postadress!');
 
+        //Check if the user only characters without the @ which is invalid
         cy.get("form");
         cy.get('input[name="username"]').type("CoolUser");
         cy.get('input[name="email"]').type("a");
         cy.get("form").submit();
         cy.url().should('eq', 'http://127.0.0.1:5500/register.html?Felformatterad%20e-postadress!');
 
+        //Check if the user start with the . and no other characters before which is invalid
         cy.get("form");
         cy.get('input[name="username"]').type("CoolUser");
         cy.get('input[name="email"]').type(".se");
         cy.get("form").submit();
         cy.url().should('eq', 'http://127.0.0.1:5500/register.html?Felformatterad%20e-postadress!');
 
+        //Check if the user types in an email but which a character after the @ which is not a letter or number
         cy.get("form");
         cy.get('input[name="username"]').type("CoolUser");
         cy.get('input[name="email"]').type("cooluser@£.se");
         cy.get("form").submit();
         cy.url().should('eq', 'http://127.0.0.1:5500/register.html?Felformatterad%20e-postadress!');
         
+        //Check if the user types in an email without anything after the . which is not a letter or number
         cy.get("form");
         cy.get('input[name="username"]').type("CoolUser");
         cy.get('input[name="email"]').type("cooluser@email.[");
@@ -63,12 +72,15 @@ describe("Account registration form", () => {
 
     });
 
+    //Test for an invalid password match
     it("Password Validation", () => {
+
         cy.visit("/");
         cy.get("a").click();
 
         cy.url().should('eq', 'http://127.0.0.1:5500/register.html');
 
+        //Test when the user only types a password in the first input tag and not in the confirm password tag and then presses the submitbutton
         cy.get("form");
         cy.get('input[name="username"]').type("CoolUser");
         cy.get('input[name="email"]').type("cooluser@email.se");
@@ -76,6 +88,7 @@ describe("Account registration form", () => {
         cy.get("form").submit();
         cy.url().should('eq','http://127.0.0.1:5500/register.html?L%C3%B6senorden%20%C3%B6verensst%C3%A4mmer%20inte!');
 
+        //Test when the uses types in a password in both the input tags but doesn't match and then presses the submitbutton
         cy.get("form");
         cy.get('input[name="username"]').type("CoolUser");
         cy.get('input[name="email"]').type("cooluser@email.se");
@@ -86,12 +99,14 @@ describe("Account registration form", () => {
 
     });
 
+    //Test that run through a sucessfull registration of an account
     it("Sucessfull registration of an account", () => {
+
         cy.visit("/");
         cy.get("a").click();
 
         cy.url().should('eq', 'http://127.0.0.1:5500/register.html');
-
+        
         cy.get("form");
         cy.get('input[name="username"]').type("CoolUser");
         cy.get('input[name="email"]').type("firstname@email.se");
