419f26db4b59d21c0cfce7ab39a7f2b87d92dcd2
FilipAlmstedt <filip.almstedt@almxer.se>
2021-01-28 17:32:06 +0100

    Added a first test for startsite

--- cypress/integration/Register.js
+++ cypress/integration/Register.js
@@ -102,4 +102,4 @@ describe("Account registration form", () => {
 
     });
 
-})
+});
--- cypress/integration/StartSite.js
+++ cypress/integration/StartSite.js
@@ -3,19 +3,19 @@ describe("Testing the links to the other html-files work", () => {
     it("Link validation which goes to the right path", () => {
        
         cy.visit("/start.html");
-        cy.url().should('eq', 'http://127.0.0.1:5500/start.html');
+        cy.url().should('include', '/start.html');
 
 
         /*cy.get("ul").children();
         //Check the first element in the list and click on the link
         cy.get("li a:first").click();
         //Check that the right site was linked
-        cy.url().should('eq', 'http://127.0.0.1:5500/guestbook.html');
+        cy.url().should('include', '/guestbook.html');
         cy.get("h3 a").click();
 
         cy.get("ul").children();
         cy.get("li").eq(1).click();
-        cy.url().should('eq', 'http://127.0.0.1:5500/presentation.html');
+        cy.url().should('include', '/presentation.html');
         cy.get("h3 a").click();
 
         cy.get("ul").children();
