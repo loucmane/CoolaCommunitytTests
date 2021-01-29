af706e0b1d8d97cd7c6b01a9ba870d1bc9c2c218
FilipAlmstedt <filip.almstedt@almxer.se>
2021-01-29 12:45:23 +0100

    Added test to validate links and url on start.html

--- cypress/integration/StartSite.js
+++ cypress/integration/StartSite.js
@@ -6,28 +6,25 @@ describe("Testing the links to the other html-files work", () => {
         cy.url().should('include', '/start.html');
 
 
-        /*cy.get("ul").children();
+        cy.get("ul").children();
         //Check the first element in the list and click on the link
         cy.get("li a:first").click();
-        //Check that the right site was linked
+        //Check that the right url was linked
         cy.url().should('include', '/guestbook.html');
         cy.get("h3 a").click();
 
         cy.get("ul").children();
+        //Check the second eleement in the ul list
         cy.get("li").eq(1).click();
+        //Check that the right url was linked
         cy.url().should('include', '/presentation.html');
         cy.get("h3 a").click();
 
+        //Check the last element in the ul list
         cy.get("ul").children();
         cy.get("li a:last").click();
 
-        cy.visit("/start.html").end();   */
-
-        cy.get("ul>li").each((index) => {
-            cy.get("a").click();
-            
-
-        })
+        cy.go("back").end();   
         
     });
 
