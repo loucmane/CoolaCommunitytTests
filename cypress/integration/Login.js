4bd2d8cd94fa2b819ff31623326a1fe6049cdf1b
Loucmane <lookmanbenali@gmail.com>
2021-01-29 17:26:38 +0100

    made tests for login and log out page.

--- cypress/integration/Login.js
+++ cypress/integration/Login.js
@@ -1,25 +1,25 @@
 describe("Login form", () => {
 
-    it("Can sign in", () => {
+    it("Is able to sign in with the correct credentials", () => {
 
-        cy.visit("/");
-        cy.get("form");
-        cy.get('input[name="username"]').type("CoolUser").should("have.value", "CoolUser");
-        cy.get('input[name="password"]').type("123123123").should("have.value", "123123123");
-        cy.get("form").submit();
-        cy.url().should("include", "start").end();
+        cy.visit('/')
+        cy.get('form')
+        cy.get('#username').type('CoolUser').should('have.value', 'CoolUser');
+        cy.get('#password').type('123123123').should('have.value', '123123123');
+        cy.get('form').submit();
+        cy.url().should('include', 'start').end(); 
+
 
     });
 
-    it("Can't sign in with wrong credentials", () => {
-
-        cy.visit("/");
-        cy.get("form");
-        cy.get('input[name="username"]').type("CoolUser");
-        cy.get('input[name="password"]').type("asd");
-        cy.get("form").submit();
-        cy.contains("Fel användarnamn eller lösenord!").end();
-
+    it("Is not able to sign with the wrong credentials", () => {
+        cy.visit('/')
+        cy.get('form')
+        cy.get('#username').type('CoolUser').should('have.value', 'CoolUser');
+        cy.get('#password').type('1231231333')
+        cy.get('form').submit();
+        cy.contains('Fel användarnamn eller lösenord').end()
+       
 
     });
 
